dd73dd1b8589ba28327518f260d4a27a3653de06
susietheginger <103440063+susietheginger@users.noreply.github.com>
2022-04-24 23:22:50 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/13g9i1BXVQnUGjZw-JYZG9Z-is7C7S6PL/edit?usp=sharing&ouid=113920797770487360939&rtpof=true&sd=true';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'favorable-logic-348221';
